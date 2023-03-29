76f3d33dd1d8fcf65af3e2210d5063dffb0e1105
JosiahElder <@JosiahElder>
2023-03-28 23:08:30 -0700

    Next Commit

--- src/Commissioned.java
+++ src/Commissioned.java
@@ -1,2 +1,16 @@
-public class Commissioned {
+public class Commissioned extends Employee
+{
+    private double grossSales;
+
+    public Commissioned(String name, double grossSales)
+    {
+        super(name);
+        this.grossSales = grossSales;
+    }
+
+    @Override
+    public double calculateMonthlyPay()
+    {
+        return 2000 + 0.1 * grossSales;
+    }
 }
--- src/Salaried.java
+++ src/Salaried.java
@@ -1,2 +1,16 @@
-public class Salaried {
+public class Salaried extends Employee
+{
+    private int yearsOfService;
+
+    public Salaried(String name, int yearsOfService)
+    {
+        super(name);
+        this.yearsOfService = yearsOfService;
+    }
+
+    @Override
+    public double calculateMonthlyPay()
+    {
+        return 50000 / 12  + 0.01 * yearsOfService;
+    }
 }
